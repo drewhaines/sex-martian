91745e33e8d3ee53f458733febfab78ea7fbecb4
Drew Haines <drewhaines3@gmail.com>
2021-04-01 17:14:58 -0700

    remove out dir

--- out/_next/static/CeHMpqcJjNtQJojiXGtnw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-f2a156be71723a0c8350.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e7a3215f1441973bc417.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
