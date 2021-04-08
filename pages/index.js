40c282bddcdc8fa61e6875768f6ea64995d74e08
matthewcraigburke <82126153+matthewcraigburke@users.noreply.github.com>
2021-04-07 19:15:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -92,7 +92,7 @@ export default function Home({ instagramPosts }) {
           >
      
                   
-                </Heading>
+               
                 <Heading
                   variant="h2"
                   mb={2}
